2c52bab6039b987e4cc5febafa0067288f193eac
staccblox <67884277+staccblox@users.noreply.github.com>
2024-10-04 14:39:42 -0400

    Update oop_maker.pas

--- oop_maker.pas
+++ oop_maker.pas
@@ -1,119 +1,24 @@
-unit Unit1;
-
-{$mode objfpc}{$H+}
-
-interface
-
-uses
-  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  Windows, ActiveX, ComObj;
-
-type
-
-  { TForm1 }
-
-  TForm1 = class(TForm, IDropTarget)
-    Memo1: TMemo;
-    procedure FormCreate(Sender: TObject);
-    procedure FormDestroy(Sender: TObject);
-  private
-    // IDropTarget
-    function DragEnter(const dataObj: IDataObject; grfKeyState: DWORD; pt: TPoint; var dwEffect: DWORD): HResult;StdCall;
-    function DragOver(grfKeyState: DWORD; pt: TPoint; var dwEffect: DWORD): HResult;StdCall;
-    function DragLeave: HResult;StdCall;
-    function Drop(const dataObj: IDataObject; grfKeyState: DWORD; pt: TPoint; var dwEffect: DWORD):HResult;StdCall;
-    // IUnknown
-    // Ignore referance counting
-    function _AddRef: Integer; stdcall;
-    function _Release: Integer; stdcall;
-  public
-    { public declarations }
-  end;
-
-
+program OOP_M(input, output);
+uses crt;
 var
-  Form1: TForm1;
-
-implementation
-
-{$R *.lfm}
-
-{ TForm1 }
-
-procedure TForm1.FormCreate(Sender: TObject);
-begin
-  OleInitialize(nil);
-  OleCheck(RegisterDragDrop(Handle, Self));
-end;
-
-procedure TForm1.FormDestroy(Sender: TObject);
-begin
-  RevokeDragDrop(Handle);
-  OleUninitialize;
-end;
-
-function TForm1.DragEnter(const dataObj: IDataObject; grfKeyState: DWORD;
-  pt: TPoint; var dwEffect: DWORD): HResult; StdCall;
-begin
-  dwEffect := DROPEFFECT_COPY;
-  Result := S_OK;
-end;
-
-function TForm1.DragOver(grfKeyState: DWORD; pt: TPoint; var dwEffect: DWORD
-  ): HResult; StdCall;
-begin
-  dwEffect := DROPEFFECT_COPY;
-  Result := S_OK;
-end;
-
-function TForm1.DragLeave: HResult; StdCall;
-begin
-  Result := S_OK;
-end;
-
-function TForm1._AddRef: Integer; stdcall;
-begin
-   Result := 1;
-end;
-
-function TForm1._Release: Integer; stdcall;
-begin
-   Result := 1;
-end;
-
-function TForm1.Drop(const dataObj: IDataObject; grfKeyState: DWORD;
-  pt: TPoint; var dwEffect: DWORD): HResult; StdCall;
+	class_name: char;
 var
-  aFmtEtc: TFORMATETC;
-  aStgMed: TSTGMEDIUM;
-  pData: PChar;
+	attributes: int;
+var
+	methods: int;
+
 begin
-  {Make certain the data rendering is available}
-  if (dataObj = nil) then
-    raise Exception.Create('IDataObject-Pointer is not valid!');
-  with aFmtEtc do
-  begin
-    cfFormat := CF_TEXT;
-    ptd := nil;
-    dwAspect := DVASPECT_CONTENT;
-    lindex := -1;
-    tymed := TYMED_HGLOBAL;
-  end;
-  {Get the data}
-  OleCheck(dataObj.GetData(aFmtEtc, aStgMed));
-  try
-    {Lock the global memory handle to get a pointer to the data}
-    pData := GlobalLock(aStgMed.hGlobal);
-    { Replace Text }
-    Memo1.Text := pData;
-  finally
-    {Finished with the pointer}
-    GlobalUnlock(aStgMed.hGlobal);
-    {Free the memory}
-    ReleaseStgMedium(aStgMed);
-  end;
-  Result := S_OK;
-end;
-
-
-end.
+   writeln("(creates js class)");
+   readkey;
+   writeln("Class name?");
+   readLn(input, class_name);
+   writeln("# of attributes?");
+   readLn(input, attributes);
+   writeln("# of methods?");
+   readLn(input, methods);
+   writeln("...");
+   writeln(class_name, " {");
+   for i:= 1 to attributes do writeln("     attribute", i, "() {}");
+   for i:= 1 to methods do writeln("     method", i, "() {}");
+   writeln("}");
+end. 
